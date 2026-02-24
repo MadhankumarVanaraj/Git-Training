d2604c30fc6f82d8a49f9c9ffcb94bd20e3d8f78
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2026-02-24 15:18:34 +0530

    Written open chrome code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,3 +3,14 @@ import {test} from '@playwright/test';
 test('Open chrome', async ({page})=>{
     await page.goto('https://www.google.com/'); 
 });
+
+test('Open chrome', async ({page})=>{
+    await page.goto('https://www.google.com/'); 
+});
+test('Open chrome', async ({page})=>{
+    await page.goto('https://www.google.com/'); 
+});
+
+test('Open chrome', async ({page})=>{
+    await page.goto('https://www.google.com/'); 
+});
