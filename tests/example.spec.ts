5a770449c4902843fa859570b3ad390eb33ac6f8
MadhankumarVanaraj <kumar.madhanmca@gmail.com>
2026-02-24 15:22:19 +0530

    Update example.spec.ts
    changes done in example code

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,14 +3,3 @@ import {test} from '@playwright/test';
 test('Open chrome', async ({page})=>{
     await page.goto('https://www.google.com/'); 
 });
-
-test('Open chrome', async ({page})=>{
-    await page.goto('https://www.google.com/'); 
-});
-test('Open chrome', async ({page})=>{
-    await page.goto('https://www.google.com/'); 
-});
-
-test('Open chrome', async ({page})=>{
-    await page.goto('https://www.google.com/'); 
-});
